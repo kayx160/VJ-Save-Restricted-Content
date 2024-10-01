89bec42e0e39069f9f979e59bad82e4640093de7
kayx160 <zaolikai@gmail.com>
2024-10-01 19:47:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7061133503:AAE1zn0mh5nja6kug_lN5xHYhbA1-v5N5Gw")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "23718593"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "cf15bbd759b060724cc4b2155b9ca42c")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "BQFp6sEAX2y6Z6cnfVLoP5sfPCo0ltrOObDRu061LM2ePXf2z0Svpe7STJr0fPraemgRJHzWyW0-ckuOnw7HaZsCNZcJtgnAmXC1NIeeyR1tf5lq7nC3TssLHlSsnXmsBMwjzpjxljoEGALd6cCzyxKPvOYBlYVRZxqUcVtOkuPB1R3dCUUqAIoeCsaljjtYKhDDhJy53jVREbUVJYaU05i0bvt1bXIMvUFxIdqo42x9RfQ-FIn6vIc3ikzoqunanw6UOQFyWrXcqsOEKSYrN6FNOOeYcYSIDT8Jcc9p8_ZC5b2c1YieY6xcjkPwtx1I-oN7bjXB6EIBXkEs0Z_TicmPP8RGOgAAAAGk4Fi_AQ")
